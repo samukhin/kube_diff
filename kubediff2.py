e8e9d361585d759a4b3f1d8a29be4c0422db158e
samukhin <47834890+samukhin@users.noreply.github.com>
2022-09-15 20:03:25 +0300

    Update kubediff2.py

--- kubediff2.py
+++ kubediff2.py
@@ -1,16 +1,24 @@
 import yaml
 
+helm_d = {}
+
 #helm template test . -n namespace > helm.txt
 with open("helm.txt", 'r') as stream:
     data_loaded = yaml.load_all(stream, yaml.FullLoader)
-    for i in data_loaded:
-        print(i['kind'], i['metadata']['name'])
+    for item in data_loaded:
+        #print(item['kind'], item['metadata']['name'])
+        helm_d[(item['kind'], item['metadata']['name'])] = item
 
-print()
+kubectl_d = {}
 
 #kubectl get all -n namespace > kubectl.txt
 with open("kubectl.txt", 'r') as stream:
     data_loaded = yaml.load_all(stream, yaml.FullLoader)
-    for i in data_loaded:
-        for j in i['items']:
-            print(j['kind'], j['metadata']['name'])
+    for items in data_loaded:
+        for item in items['items']:
+            #print(item['kind'], item['metadata']['name'])
+            kubectl_d[(item['kind'], item['metadata']['name'])] = item
+
+
+print(helm_d.keys())
+print(kubectl_d.keys())
